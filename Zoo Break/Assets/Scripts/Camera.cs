4d67104abbc71aadf120f60e60737e2ca7383b6c
Anna Kasagawa <anna.kasagawa@gmail.com>
2016-11-18 18:27:46 +0100

    dobby moving

---
+++ Zoo Break/Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	private Transform target;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 velocity = Vector3.zero;
+		Vector3 forward = target.transform.forward * 10.0f;
+		Vector3 needPos = target.transform.position - forward;
+		transform.position = Vector3.SmoothDamp(transform.position, needPos,
+			ref velocity,0.05f);
+		transform.LookAt (target.transform);
+		transform.rotation = target.transform.rotation;
+	}
+		
+}
---
+++ Zoo Break/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxSpeed;
+	bool right = true;
+
+
+	bool ground = false;
+	public Transform isGround;
+	float grRadius = 0.2f;
+	public float jumpForce = 200f;
+	public LayerMask wiGround;
+
+	Animator anim;
+	Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update() {
+		if (ground && Input.GetKeyDown (KeyCode.UpArrow)) {
+			anim.SetBool ("Ground", false);
+			rb.AddForce (new Vector2 (0,jumpForce));
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		ground = Physics2D.OverlapCircle (isGround.position, grRadius, wiGround);
+		anim.SetBool ("Ground", ground);
+		anim.SetFloat ("vSpeed", rb.velocity.y);
+
+		//make the character move 
+		float move = Input.GetAxis ("Horizontal");
+
+		anim.SetFloat("Speed", Mathf.Abs(move));
+
+		rb.velocity = new Vector2 (move * maxSpeed, rb.velocity.y);
+
+		//check which direction the character is facing
+		if (move > 0 && !right) {
+			Flip ();
+		} else if (move < 0 && right) {
+			Flip ();
+		}
+	
+
+	}
+
+	void Flip() {
+		right = !right;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}
