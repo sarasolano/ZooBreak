daa5807ffea6c696f71c90e5e4d53d74dc0fd7dc
Ashley Simons <acs32014@mymail.pomona.edu>
2016-11-21 17:24:26 +0100

    story scene, new prefabs, started level 1

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,11 +10,24 @@ public class CameraController : MonoBehaviour {
 		target = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 
+//	// Update is called once per frame
+//	void LateUpdate () {
+//		Vector3 velocity = Vector3.zero;
+//		Vector3 forward = target.transform.forward * 10.0f;
+//		Vector3 needPos = target.transform.position - forward;
+//		transform.position = Vector3.SmoothDamp(transform.position, needPos,
+//			ref velocity,0.05f);
+//		transform.LookAt (target.transform);
+//		transform.rotation = target.transform.rotation;
+//	}
+
+
 	// Update is called once per frame
 	void LateUpdate () {
 		Vector3 velocity = Vector3.zero;
+		Vector3 up = target.transform.up * 5.5f;
 		Vector3 forward = target.transform.forward * 10.0f;
-		Vector3 needPos = target.transform.position - forward;
+		Vector3 needPos = target.transform.position - forward + up;
 		transform.position = Vector3.SmoothDamp(transform.position, needPos,
 			ref velocity,0.05f);
 		transform.LookAt (target.transform);
--- Assets/Scripts/StorySceneScript.cs
+++ Assets/Scripts/StorySceneScript.cs
@@ -8,6 +8,7 @@ public class StorySceneScript : MonoBehaviour {
 	// the buttons at the bottom of the screen
 	public Button BackButton;
 	public Button AvatarButton;
+	public Text storyText;
 
 	//This initializes the button so that when you click it, you go to the next scene 
 	void Start () {
@@ -19,6 +20,10 @@ public class StorySceneScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			storyText.text = "You have managed to escape from your enclosure. " +
+				"Now, it is up to you to free your fellow humans from each of the other cages.";
+
+		}
 	}
 }
