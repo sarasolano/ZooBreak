d68507bfc76a6a5db0039fba364e7bf2cf6aab8b
Ashley Simons <acs32014@mymail.pomona.edu>
2016-11-28 20:21:09 +0100

    added new levels

---
+++ Assets/Scripts/Level4Manager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Level4Manager : MonoBehaviour {
+	public Button MapButton;
+
+	// Use this for initialization
+	void Start () {
+		MapButton.onClick.AddListener (() =>
+			SceneManager.LoadScene ("MainMap"));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		AllLevelManager.level4Over = true;
+	}
+}
