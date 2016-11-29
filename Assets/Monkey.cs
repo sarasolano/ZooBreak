82748ecc6e8c666108d3285761cc8900695017b2
Anna Kasagawa <anna.kasagawa@gmail.com>
2016-11-29 17:37:20 +0100

    six animated people

---
+++ Assets/Monkey.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monkey : MonoBehaviour {
+
+
+	private Animator anim;
+
+	public GameObject banana;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+		anim.SetTrigger ("Throw"); 
+	}
+	
+	void ThrowBanana(){
+		//AudioSource.PlayClipAtPoint(laserSound, transform.position);
+		Instantiate(banana, transform.position + Vector3.up, Quaternion.identity);
+	}
+
+}
---
+++ Assets/Scripts/Banana.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Banana : MonoBehaviour {
+
+	public float speed;
+	public float lifeTime;
+
+	void Start(){
+		StartCoroutine(KillAfterSeconds(lifeTime));
+	}
+
+	void Update () {
+		transform.position += Vector3.up * speed * Time.deltaTime;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.CompareTag("Player")){
+			//then restart level
+			Destroy(gameObject);
+		}
+	}
+
+	IEnumerator KillAfterSeconds(float seconds){
+		yield return new WaitForSeconds(seconds);
+		Destroy(gameObject);
+	}
+}
