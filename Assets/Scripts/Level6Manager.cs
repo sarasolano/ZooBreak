89fe8616864047c4fe4ca0e28d52bf70c7e0b015
Ashley Simons <acs32014@mymail.pomona.edu>
2016-11-29 13:44:12 +0100

    hopefully no merge conflict, added scene 6

---
+++ Assets/Scripts/Level6Manager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Level6Manager : MonoBehaviour {
+	public Button MapButton;
+
+	// Use this for initialization
+	void Start () {
+		MapButton.onClick.AddListener (() =>
+			SceneManager.LoadScene ("MainMap"));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		AllLevelManager.level6Over = true;
+	}
+
+	//when win --> winning scene
+}
