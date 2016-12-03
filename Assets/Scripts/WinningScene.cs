56f7c1b2e39c0ff95475ec89e4ecafa12d3cb73f
Ashley Simons <acs32014@mymail.pomona.edu>
2016-12-04 00:14:39 +0100

    added winning scene

---
+++ Assets/Scripts/WinningScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinningScene : MonoBehaviour {
+	// the restart button, which takes you to the start scene
+	public Button ReStartButton;
+
+	// Use this for initialization
+	void Start () {
+		ReStartButton.onClick.AddListener (() =>
+			SceneManager.LoadScene ("StartScene"));
+	}
+}
